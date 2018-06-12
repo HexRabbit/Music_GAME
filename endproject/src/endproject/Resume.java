67951adfe2951ce87c270907502ccba195f1241d
YuXiang <richardlin@gmail.com>
2018-06-12 12:05:04 +0800

    Fix a bug in "Resume"

--- endproject/src/endproject/Resume.java
+++ endproject/src/endproject/Resume.java
@@ -23,19 +23,19 @@ public class Resume {
 				Timer show_timer = new Timer();
 				Move move = new Move(now, now.parent);
 				Show show = new Show(now, now.parent);
-				if(now.b -2500 > real_run_time) {  //還沒出現
+				if(now.b -1000 > real_run_time) {  //還沒出現
 					now.move_timer = move_timer;
 					now.show_timer = show_timer;
-					now.move_timer.schedule(move, now.b - real_run_time - 1000, 40);
+					now.move_timer.schedule(move, now.b - real_run_time - 1000, 2);
 					now.show_timer.schedule(show, now.b - real_run_time - 1000);
 					
 				} else { //已經出現
 					now.move_timer = move_timer;
-					now.move_timer.schedule(move, 0, 40);
+					now.move_timer.schedule(move, 0, 2);
 					
 				}
 				
-				//remove_timer.schedule(remove, 0, 40);
+				//remove_timer.schedule(remove, 0, 2);
 				now = now.nextL;
 			} while(now != null);
 			
